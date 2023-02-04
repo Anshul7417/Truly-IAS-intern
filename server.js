51817b82053a2e71c3a73178e7a6f77b928b7a2a
Anshul Tripathi <tripathianshul06@gmail.com>
2023-02-04 21:02:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+// const express=require("express");
+// const app=express();
+
+
+// app.get("/",(req,res)=>{
+//     res.render(homepage)
+// })
+// app.get("/",(req,res)=>{
+//     res.send()
+// })
+// app.get("/contact",(req,res)=>{
+//     res.send("hello from contact")
+// })
+// app.get("/temp",(req,res)=>{
+//     res.status(200).send(Addques)  
+// })
+
+// app.listen(8000,"127.0.0.1",()=>{
+//     console.log("server running");
+// })
